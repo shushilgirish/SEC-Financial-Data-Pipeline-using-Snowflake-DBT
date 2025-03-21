4551e917bbda328d3a5cafc58c2a18b35f335e43
shushil girish <girish.sh@northeastern.edu>
2025-03-21 14:38:19 -0400

    json change

--- Airflow/dags/json_dbt_transformation.py
+++ Airflow/dags/json_dbt_transformation.py
@@ -1,22 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv(dotenv_path='/opt/airflow/.env')
-
-# Access environment variables for Snowflake connection
-snowflake_env_vars = {
-    'SNOWFLAKE_ACCOUNT': os.getenv('SNOWFLAKE_ACCOUNT'),
-    'SNOWFLAKE_USER': os.getenv('SNOWFLAKE_USER'),
-    'SNOWFLAKE_PASSWORD': os.getenv('SNOWFLAKE_PASSWORD'),
-    'SNOWFLAKE_ROLE': os.getenv('SNOWFLAKE_ROLE'),
-    'SNOWFLAKE_WAREHOUSE': os.getenv('SNOWFLAKE_WAREHOUSE'),
-    'SNOWFLAKE_DATABASE': os.getenv('SNOWFLAKE_DATABASE'),
-    'SNOWFLAKE_SCHEMA': os.getenv('SNOWFLAKE_SCHEMA')
-}
 
 default_args = {
     'owner': 'airflow',
@@ -33,41 +17,30 @@ dag = DAG(
     catchup=False,
 )
 
-# Define the path to your DBT project
+# Define the path to your DBT project and executable
 DBT_PROJECT_DIR = "/opt/airflow/json_transform"
-# Define the full path to the dbt executable
 DBT_EXECUTABLE = "/home/airflow/.local/bin/dbt"
 
-# Define common environment variables for DBT tasks and include DBT_PROFILES_DIR
-dbt_env = {
-    **snowflake_env_vars,
-    'DBT_PROFILES_DIR': DBT_PROJECT_DIR  # This tells dbt to use the profiles.yml in your project directory
-}
-
-# Run `dbt debug` to test the connection
+# Hard-code DBT_PROFILES_DIR in the command to ensure the correct profiles.yml is used
 dbt_debug = BashOperator(
     task_id='dbt_debug',
-    bash_command=f"cd {DBT_PROJECT_DIR} && {DBT_EXECUTABLE} debug",
-    env=dbt_env,
+    bash_command=(
+        f"cd {DBT_PROJECT_DIR} && "
+        f"export DBT_PROFILES_DIR={DBT_PROJECT_DIR} && "
+        f"echo 'DBT_PROFILES_DIR is set to:' $DBT_PROFILES_DIR && "
+        f"{DBT_EXECUTABLE} debug"
+    ),
     dag=dag,
 )
 
-# Run `dbt run` to execute models
 dbt_run = BashOperator(
     task_id='dbt_run',
-    bash_command=f"cd {DBT_PROJECT_DIR} && {DBT_EXECUTABLE} run",
-    env=dbt_env,
+    bash_command=(
+        f"cd {DBT_PROJECT_DIR} && "
+        f"export DBT_PROFILES_DIR={DBT_PROJECT_DIR} && "
+        f"{DBT_EXECUTABLE} run"
+    ),
     dag=dag,
 )
 
-# Optional: Run `dbt test` to validate transformations
-# dbt_test = BashOperator(
-#     task_id='dbt_test',
-#     bash_command=f"cd {DBT_PROJECT_DIR} && {DBT_EXECUTABLE} test",
-#     env=dbt_env,
-#     dag=dag,
-# )
-
-# DAG Execution Order
 dbt_debug >> dbt_run
-# >> dbt_test
